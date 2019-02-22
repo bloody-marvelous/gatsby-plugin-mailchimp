2debb911a86c9051ef7127bced90f8a3a63f2487
Aaron Dack <adackbusiness@gmail.com>
2019-02-21 21:36:28 -0600

    Update build

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _jsonp = require('jsonp');
+var _jsonp = require("jsonp");
 
 var _jsonp2 = _interopRequireDefault(_jsonp);
 
-var _emailValidator = require('email-validator');
+var _emailValidator = require("email-validator");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
 /*
  * make a jsonp request to user's mailchimp list
@@ -19,12 +21,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 
 var subscribeEmailToMailchimp = function subscribeEmailToMailchimp(url) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     // `param` object avoids CORS issues
     // timeout to 3.5s so user isn't waiting forever
     // usually occurs w/ privacy plugins enabled
     // 3.5s is a bit longer than the time it would take on a Slow 3G connection
-    return (0, _jsonp2.default)(url, { param: 'c', timeout: 3500 }, function (err, data) {
+    return (0,
+    _jsonp2.default)(url, { param: "c", timeout: 3500 }, function(err, data) {
       if (err) reject(err);
       if (data) resolve(data);
     });
@@ -38,36 +41,47 @@ var subscribeEmailToMailchimp = function subscribeEmailToMailchimp(url) {
  */
 
 var convertListFields = function convertListFields(fields) {
-  var queryParams = '';
+  var queryParams = "";
   for (var field in fields) {
-    queryParams = queryParams.concat('&' + field.toUpperCase() + '=' + fields[field]);
+    queryParams = queryParams.concat(
+      "&" + field.toUpperCase() + "=" + fields[field]
+    );
   }
   return queryParams;
 };
 
+var generateJSONPEndpoint = function generateJSONPEndpoint(str) {
+  return str.replace(/\/post/g, "/post-json");
+};
 /*
  * accept email (String) and additional, optional
  * Mailchimp list fields (Object)
  * then make jsonp req with data
  */
 
-var addToMailchimp = function addToMailchimp(email, fields) {
+var addToMailchimp = function addToMailchimp(
+  email,
+  fields,
+  overridableEndpoint
+) {
   var isEmailValid = (0, _emailValidator.validate)(email);
   var emailEncoded = encodeURIComponent(email);
   if (!isEmailValid) {
     return Promise.resolve({
-      result: 'error',
-      msg: 'The email you entered is not valid.'
+      result: "error",
+      msg: "The email you entered is not valid."
     });
   }
 
   // generate Mailchimp endpoint for jsonp request
   // note, we change `/post` to `/post-json`
   // otherwise, Mailchomp returns an error
-  var endpoint = __GATSBY_PLUGIN_MAILCHIMP_ADDRESS__.replace(/\/post/g, '/post-json');
+  var endpoint = overridableEndpoint
+    ? generateJSONPEndpoint(overridableEndpoint)
+    : generateJSONPEndpoint(__GATSBY_PLUGIN_MAILCHIMP_ADDRESS__);
 
-  var queryParams = '&EMAIL=' + emailEncoded + convertListFields(fields);
-  var url = '' + endpoint + queryParams;
+  var queryParams = "&EMAIL=" + emailEncoded + convertListFields(fields);
+  var url = "" + endpoint + queryParams;
   return subscribeEmailToMailchimp(url);
 };
 
